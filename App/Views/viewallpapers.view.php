756e50ee4c4fd1321e36fd8c82b54d68e8a145eb
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-30 00:34:08 +0300

    comment

--- App/Views/viewallpapers.view.php
+++ App/Views/viewallpapers.view.php
@@ -127,6 +127,8 @@
                 <li><a href="#">»</a></li>
               </ul>
             </div> -->
+                                <a href="/commentpost/">
+                                    <button type="button" class="btn btn-secondary">Comments</button>
                             </div>
                         </div>
                     </div>

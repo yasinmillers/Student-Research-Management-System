e8731076dccf8092b7d76a0091c58e7e23c41e57
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-29 12:01:38 +0300

    changes

--- App/Controllers/ProfileController.php
+++ App/Controllers/ProfileController.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
- * @author  Fahad Yousaf Mahar https://fb.com/fahad3267
- * @website https://www.github.com/fahadyousafmahar
+ * @author  ssenyongayasin
+ * @website https://www.github.com/yasinmillers
  **/
+
 namespace Myweb\Controllers;
 
 use Myweb\Core\App;
@@ -34,20 +36,20 @@ class ProfileController
                     $pageTitle = 'About';
                     return view('aboutprofile', compact('title', 'pageTitle', 'user', 'about'));
                 } else {
-                    $e = new Exception("Empty About at ".get_class()."@".__FUNCTION__." in ".__FILE__." at ".__LINE__."\n");
-                    error_log("My Error : ".$e->getMessage());
+                    $e = new Exception("Empty About at " . get_class() . "@" . __FUNCTION__ . " in " . __FILE__ . " at " . __LINE__ . "\n");
+                    error_log("My Error : " . $e->getMessage());
                     nextpagealert("error", "User's About Page Does Not Exist");
                     header("Location: /login");
                 }
             } else {
-                $e = new Exception("Empty User at ".get_class()."@".__FUNCTION__." in ".__FILE__." at ".__LINE__."\n");
-                error_log("My Error : ".$e->getMessage());
+                $e = new Exception("Empty User at " . get_class() . "@" . __FUNCTION__ . " in " . __FILE__ . " at " . __LINE__ . "\n");
+                error_log("My Error : " . $e->getMessage());
                 //nextpagealert("error","User Does Not Exist");
                 header("Location: /login");
             }
         } catch (PDOException $e) {
             $e->getMessage();
-            error_log("DB Error : ".$e->getMessage());
+            error_log("DB Error : " . $e->getMessage());
         }
     }
 
@@ -56,8 +58,8 @@ class ProfileController
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
-        
-        if (isset($_SESSION['email']) && $_SESSION['type']=='user') {
+
+        if (isset($_SESSION['email']) && $_SESSION['type'] == 'user') {
             try {
                 if (!isset($username)) {
                     $username = $_SESSION['email'];
@@ -66,7 +68,7 @@ class ProfileController
                 if (count($result) > 0) {
                     $user =  $result[0];
                     $about = App::get('database')->query('about', 'About', "where userid = $user->id ");
-                    if (count($about)>0) {
+                    if (count($about) > 0) {
                         $about = $about[0];
                     } else {
                         $about = new \Myweb\Classes\About;
@@ -78,14 +80,14 @@ class ProfileController
                     $pageTitle = 'About';
                     return view('youraccount', compact('title', 'pageTitle', 'user', 'about'));
                 } else {
-                    $e = new Exception("Empty User at ".get_class()."@".__FUNCTION__." in ".__FILE__." at ".__LINE__."\n");
-                    error_log("My Error : ".$e->getMessage());
+                    $e = new Exception("Empty User at " . get_class() . "@" . __FUNCTION__ . " in " . __FILE__ . " at " . __LINE__ . "\n");
+                    error_log("My Error : " . $e->getMessage());
                     nextpagealert("error", "User Does Not Exist");
                     header("Location: /login");
                 }
             } catch (PDOException $e) {
                 $e->getMessage();
-                error_log("DB Error : ".$e->getMessage());
+                error_log("DB Error : " . $e->getMessage());
             }
         } else {
             header("Location: /login");
@@ -97,8 +99,8 @@ class ProfileController
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
-        
-        if (isset($_SESSION['email']) && $_SESSION['type']=='user') {
+
+        if (isset($_SESSION['email']) && $_SESSION['type'] == 'user') {
             try {
                 if (!isset($username)) {
                     $username = $_SESSION['email'];
@@ -117,14 +119,14 @@ class ProfileController
                     $pageTitle = 'Friends\' Birthdays';
                     return view('friendbirthday', compact('title', 'pageTitle', 'friends'));
                 } else {
-                    $e = new Exception("Empty User at ".get_class()."@".__FUNCTION__." in ".__FILE__." at ".__LINE__."\n");
-                    error_log("My Error : ".$e->getMessage());
+                    $e = new Exception("Empty User at " . get_class() . "@" . __FUNCTION__ . " in " . __FILE__ . " at " . __LINE__ . "\n");
+                    error_log("My Error : " . $e->getMessage());
                     nextpagealert("error", "You have no Friends. Try to make some !");
                     header("Location: /login");
                 }
             } catch (PDOException $e) {
                 $e->getMessage();
-                error_log("DB Error : ".$e->getMessage());
+                error_log("DB Error : " . $e->getMessage());
             }
         } else {
             header("Location: /login");
@@ -136,8 +138,8 @@ class ProfileController
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
-        
-        if (isset($_SESSION['email']) && $_SESSION['type']=='user') {
+
+        if (isset($_SESSION['email']) && $_SESSION['type'] == 'user') {
             try {
                 if (!isset($username)) {
                     $username = $_SESSION['email'];
@@ -158,14 +160,14 @@ class ProfileController
                     $pageTitle = 'Friends';
                     return view('friends', compact('title', 'pageTitle', 'friends', 'user'));
                 } else {
-                    $e = new Exception("Empty User at ".get_class()."@".__FUNCTION__." in ".__FILE__." at ".__LINE__."\n");
-                    error_log("My Error : ".$e->getMessage());
+                    $e = new Exception("Empty User at " . get_class() . "@" . __FUNCTION__ . " in " . __FILE__ . " at " . __LINE__ . "\n");
+                    error_log("My Error : " . $e->getMessage());
                     nextpagealert("error", "You have no Friends. Try to make some !");
                     header("Location: /timeline");
                 }
             } catch (PDOException $e) {
                 $e->getMessage();
-                error_log("DB Error : ".$e->getMessage());
+                error_log("DB Error : " . $e->getMessage());
             }
         } else {
             header("Location: /login");
@@ -180,40 +182,40 @@ class ProfileController
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
-        
-        if (isset($_SESSION['email']) && $_SESSION['type']=='user') {
+
+        if (isset($_SESSION['email']) && $_SESSION['type'] == 'user') {
             try {
                 $result = App::get('database')->selectOne('user', 'User', 'id', $_SESSION['id']);
                 $user = $result[0];
-                $result = App::get('database')->query('user','User',"where id in (select user_one_id from friends where user_two_id = {$_SESSION['id']} and status = 1) or id in (select user_two_id from friends where user_one_id = {$_SESSION['id']} and status = 1)");
+                $result = App::get('database')->query('user', 'User', "where id in (select user_one_id from friends where user_two_id = {$_SESSION['id']} and status = 1) or id in (select user_two_id from friends where user_one_id = {$_SESSION['id']} and status = 1)");
                 $friends = $result;
                 $result = App::get('database')->query('messages', 'Message', "where fromid in ( {$_SESSION['id']} ) or toid in ( {$_SESSION['id']} ) order by createdat desc");
                 $messages = $result;
                 if (!isset($username)) {
                     try {
                         $result = App::get('database')->customselect("(select fromid from messages where toid = {$_SESSION['id']} order by createdat desc) union (select toid from messages where fromid = {$_SESSION['id']} order by createdat desc )");
-                        if(!empty($result[0]['fromid'])){
+                        if (!empty($result[0]['fromid'])) {
                             $chatuserid = $result[0]['fromid'];
-                        }else{
+                        } else {
                             $chatuserid = $result[0]['toid'];
                         }
                     } catch (PDOException $e) {
                         $e->getMessage();
-                        error_log("DB Error : ".$e->getMessage());
+                        error_log("DB Error : " . $e->getMessage());
                     }
-                    return view('messages', compact('title', 'pageTitle', 'friends', 'user', 'messages','chatuserid'));
+                    return view('messages', compact('title', 'pageTitle', 'friends', 'user', 'messages', 'chatuserid'));
                 } else {
                     if (!empty($chatuserid = $this->find('username', $username, 'id', $friends))) {
-                            return view('messages', compact('title', 'pageTitle', 'friends', 'user', 'messages','chatuserid'));
+                        return view('messages', compact('title', 'pageTitle', 'friends', 'user', 'messages', 'chatuserid'));
                     } else {
                         $result = App::get('database')->customselect("(select fromid from messages where toid = {$_SESSION['id']} order by createdat desc) union (select toid from messages where fromid = {$_SESSION['id']} order by createdat desc )");
                         $chatuserid = $result[0];
-                        return view('messages', compact('title', 'pageTitle', 'friends', 'user', 'messages','chatuserid'));
+                        return view('messages', compact('title', 'pageTitle', 'friends', 'user', 'messages', 'chatuserid'));
                     }
                 }
             } catch (PDOException $e) {
                 $e->getMessage();
-                error_log("DB Error : ".$e->getMessage());
+                error_log("DB Error : " . $e->getMessage());
             }
         } else {
             header("Location: /login");
@@ -226,8 +228,8 @@ class ProfileController
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
-        
-        if (isset($_SESSION['email']) && $_SESSION['type']=='user') {
+
+        if (isset($_SESSION['email']) && $_SESSION['type'] == 'user') {
             $title = 'Change Password';
             $pageTitle = 'Change Password';
             return view('changepassword', compact('title', 'pageTitle'));
@@ -242,15 +244,15 @@ class ProfileController
             session_start();
         }
         $_POST = Gump::sanitize(Gump::xss_clean($_POST));
-        if (isset($_SESSION['email']) && $_SESSION['type']=='user') {
-            foreach ($_POST as $key=>$value) {
+        if (isset($_SESSION['email']) && $_SESSION['type'] == 'user') {
+            foreach ($_POST as $key => $value) {
                 if (\array_key_exists($key, \get_object_vars(new \Myweb\Classes\User))) {
-                    $user[$key]=$value;
+                    $user[$key] = $value;
                 } else {
-                    $about[$key]=$value;
+                    $about[$key] = $value;
                 }
             }
-            $user['birthday']= date("Y-m-d", strtotime($user['birthday']));
+            $user['birthday'] = date("Y-m-d", strtotime($user['birthday']));
             try {
                 if (App::get('database')->updatewhere('user', $user, 'id', $_SESSION['id']) && App::get('database')->updatewhere('about', $about, 'userid', $_SESSION['id'])) {
                     nextpagealert("success", "Profile was Updated Successfully!");
@@ -261,7 +263,7 @@ class ProfileController
                 }
             } catch (PDOException $e) {
                 nextpagealert("error", "A database error occurred in Updating profile!");
-                \error_log("DB Error : ".$e->getMessage());
+                \error_log("DB Error : " . $e->getMessage());
                 header("Location: /setting");
             }
         }

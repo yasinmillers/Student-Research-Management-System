f4fb804ebc4c028ee8e24866a5d87567c7172b4d
Ssenyonga yasin <ssenyonga100@gmail.com>
2022-03-29 11:56:52 +0300

    changes

--- App/Controllers/PagesController.php
+++ App/Controllers/PagesController.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * @author  Fahad Yousaf Mahar https://fb.com/fahad3267
- * @website https://www.github.com/fahadyousafmahar
- **/
+
 namespace Myweb\Controllers;
 
 use Myweb\Core\App;
@@ -18,8 +15,8 @@ class PagesController
             session_start();
         }
         if (isset($_SESSION['email']) && $_SESSION["type"]) {
-            header("Location: /dashboard"); 
-        }else{
+            header("Location: /dashboard");
+        } else {
 
             $title = 'Home ';
             return view('index', compact('title'));
@@ -33,10 +30,9 @@ class PagesController
             session_start();
         }
         if (isset($_SESSION['email']) && isset($_SESSION['type'])) {
-                header("Location: /".$_SESSION['type']);
+            header("Location: /" . $_SESSION['type']);
         } else {
             header('Location: /login');
-
         }
     }
 
@@ -54,17 +50,17 @@ class PagesController
         }
     }
 
-    
+
     public function loginProcess()
     {
-        
+
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
         $errors = array();
         $status = false;
-        if(isset($_POST['type']) && !empty($_POST['email']) && !empty($_POST['password'])){
-                try {
+        if (isset($_POST['type']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+            try {
                 $row = App::get('database')->selectOne(strtolower($_POST['type']), $_POST['type'], 'email', $_POST['email']);
                 if (isset($row[0])) {
                     if ($_POST['email'] == $row[0]->email && sha1(md5($_POST['password'])) == $row[0]->password) {
@@ -72,23 +68,23 @@ class PagesController
                             $_SESSION[$key] = $value;
                         }
                         $_SESSION['type'] = $_POST['type'];
-                        \header("Location: /dashboard"); 
+                        \header("Location: /dashboard");
                     } else {
                         $status = false;
                         array_push($errors, "Eamil or Password do not match.");
-                        nextpagealert("error","Email or Password do not match.");
+                        nextpagealert("error", "Email or Password do not match.");
                     }
                 } else {
                     array_push($errors, "No account exists with that email");
-                    nextpagealert("error","No account exists with that email");
+                    nextpagealert("error", "No account exists with that email");
                 }
             } catch (PDOException $e) {
                 $e->getMessage();
-                error_log("DB Error : ".$e->getMessage());
+                error_log("DB Error : " . $e->getMessage());
             }
-        }else{
+        } else {
             array_push($errors, "Please fill all fields.");
-            nextpagealert("error","Please fill all fields.");
+            nextpagealert("error", "Please fill all fields.");
         }
         $status = false;
         $title  = 'Login';
@@ -101,14 +97,14 @@ class PagesController
         if (ini_get("session.use_cookies")) {
             $params = session_get_cookie_params();
             setcookie(
-          session_name(),
-          '',
-          time() - 42000,
-          $params["path"],
-          $params["domain"],
-          $params["secure"],
-          $params["httponly"]
-      );
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
         }
         header('Location: /login');
     }
@@ -120,17 +116,17 @@ class PagesController
             session_start();
         }
         if (isset($_SESSION['email']) && isset($_SESSION['type'])) {
-                try {
-                    $row = App::get('database')->selectOne(strtolower($_SESSION['type']), $_SESSION['type'], 'id', $_SESSION['id']);
-                    $user = $row[0];
-                    $title  = 'My Profile';
-                    return view('myprofile', compact('title', 'user'));
-                } catch (PDOException $e) {
-                    $e->getMessage();
-                    error_log("DB Error : ".$e->getMessage());
-                }
-        }else {
-            \nextpagealert("success","Please Login !");
+            try {
+                $row = App::get('database')->selectOne(strtolower($_SESSION['type']), $_SESSION['type'], 'id', $_SESSION['id']);
+                $user = $row[0];
+                $title  = 'My Profile';
+                return view('myprofile', compact('title', 'user'));
+            } catch (PDOException $e) {
+                $e->getMessage();
+                error_log("DB Error : " . $e->getMessage());
+            }
+        } else {
+            \nextpagealert("success", "Please Login !");
             header('Location: /login');
         }
     }
@@ -145,7 +141,8 @@ class PagesController
         return view('404', compact('title'));
     }
 
-        public function phpinfo(){
-            phpinfo();
-        }
+    public function phpinfo()
+    {
+        phpinfo();
+    }
 }
